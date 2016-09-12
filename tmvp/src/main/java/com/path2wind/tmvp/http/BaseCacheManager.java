675497329f2546117b9c232e172d7a8d78226072
am_faith <path2wind@gmail.com>
2016-09-12 22:20:18 +0800

    201609122220

---
+++ tmvp/src/main/java/com/path2wind/tmvp/http/BaseCacheManager.java
@@ -0,0 +1,9 @@
+package com.path2wind.tmvp.http;
+
+/**
+ * @author Terry
+ * @time 16/9/12 21:49
+ * email path2wind@gmail.com
+ */
+public abstract class BaseCacheManager {
+}
---
+++ tmvp/src/main/java/com/path2wind/tmvp/http/GlobeHttpResultHandler.java
@@ -0,0 +1,10 @@
+package com.path2wind.tmvp.http;
+
+/**
+ * @author Terry
+ * @time 16/9/12 21:50
+ * email path2wind@gmail.com
+ */
+public interface GlobeHttpResultHandler {
+
+}
