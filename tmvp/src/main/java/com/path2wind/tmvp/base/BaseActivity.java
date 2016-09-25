f2b60649ef894740d958ec3f139be5d1937967d5
am_faith <path2wind@gmail.com>
2016-09-25 21:15:58 +0800

    201609252115

---
+++ tmvp/src/main/java/com/path2wind/tmvp/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.path2wind.tmvp.base;
+
+import com.path2wind.tmvp.mvp.BasePresenter;
+
+/**
+ * @author Terry
+ * @time 16/9/25 21:13
+ * email path2wind@gmail.com
+ */
+public abstract class BaseActivity<P extends BasePresenter> extends RxAppCompatActivity {
+
+}
---
+++ tmvp/src/main/java/com/path2wind/tmvp/base/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.path2wind.tmvp.base;
+
+import android.app.Application;
+
+/**
+ * @author Terry
+ * @time 16/9/25 21:15
+ * email path2wind@gmail.com
+ */
+public class BaseApplication extends Application {
+
+    
+}
