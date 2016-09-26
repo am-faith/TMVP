ababef35c0ff0b164693f569e05634aaa1dff5af
am_faith <path2wind@gmail.com>
2016-09-26 17:17:36 +0800

    201609261717

---
+++ tmvp/src/main/java/com/path2wind/tmvp/retrofit/BaseInterceptor.java
@@ -0,0 +1,11 @@
+package com.path2wind.tmvp.retrofit;
+
+/**
+ * @author Terry
+ * @time 16/9/26 16:56
+ * email path2wind@gmail.com
+ */
+public class BaseInterceptor implements Interceptor {
+
+
+}
